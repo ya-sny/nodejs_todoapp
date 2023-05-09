61a6fc6c74ce2cea8f1a1ce7285f8f1ed378de4a
hardcore <>
2023-05-09 01:25:31 +0000

    install express nodemon

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 const express = require("express");
 const app = express();
-const taskRoute = require("./routes/tasks");
-const connectDB = require("./db/connect");
-require("dotenv").config();
-
-app.use(express.json());
-app.use(express.static("./public"));
-
 const PORT = 3000;
 
-//ルーティング設計
-app.use("/api/v1/tasks", taskRoute);
-
-//データベースと接続
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
-    app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-start();
+app.listen(PORT, console.log("server start"));
