4b2a2219635c3656154d535c1723516f2bf89f94
hardcore <>
2023-05-09 05:11:33 +0000

    create task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,18 @@
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 }
 
-const createTask = (req, res) => {
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const createTask = await create(req.body);
+    res.status(200).json(createTask);
+  }
+  catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 }
 
 const getSingleTask = (req, res) => {
