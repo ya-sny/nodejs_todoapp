a5abc185bc7866b64d3fd313f82335afd32b69a0
hardcore <>
2023-05-09 01:50:22 +0000

    HTTP method

--- app.js
+++ app.js
@@ -2,4 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/api/v1/tasks", (req, res) => {
+  res.send("get all tasks");
+});
+
+app.post("/api/v1/tasks", (req, res) => {
+  res.send("create task");
+})
+
+app.get("/api/v1/tasks/:id", (req, res) => {
+  res.send("get single task");
+})
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("update single task");
+})
+
+app.delete("/api/v1/tasks/:id", (req, res) => {
+  res.send("delete single task");
+})
+
 app.listen(PORT, console.log("server start"));
