cbd0bfb36ffc260861dc2dbb886b69fea6bdf8a0
hardcore <>
2023-05-11 00:35:20 +0000

    showTasks

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log(axios);
+const showTasks = async () => {
+  try {
+    //自作のAPIを叩く
+    const {data : tasks}= await axios.get("/api/v1/tasks");
+    console.log(tasks);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+showTasks();
