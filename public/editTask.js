8e0179cfb7b22a03b897cebb151dfafe5299f919
hardcore <>
2023-05-11 02:27:44 +0000

    get task info

--- public/editTask.js
+++ public/editTask.js
@@ -1,3 +1,18 @@
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
-console.log(id);
+const taskIDDOM = document.querySelector(".task-edit-id");
+const taskNameDOM = document.querySelector(".task-edit-name");
+
+const showTask = async () => {
+  try {
+    const {data: task} = await axios.get(`/api/v1/tasks/${id}`);
+    const {_id, completed, name} = task;
+    taskIDDOM.textContent = _id;
+    taskNameDOM.value = name;
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+showTask();
